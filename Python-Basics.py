8c33f2e428ad893f5d9ee9585b48997b6af3e4a7
Syriamanal <Shadinachat123@gmail.com>
2013-12-27 12:46:50 -0800

    Create Python-Basics.py

---
+++ Python-Basics.py
@@ -0,0 +1,9 @@
+fish = "tuna"                 
+if fish == "tuna":            
+    print 'This is real tuna'  
+elif: fish == "Salmon":
+    print 'I hope i didnt eat that'
+elif: fish == shark:
+    print 'I hope it doesnt eat me lol'
+else:
+    print 'I dont know what this is'
