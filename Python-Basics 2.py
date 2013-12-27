2984ea748311157f776142a95a511e97ffbe6da6
Syriamanal <Shadinachat123@gmail.com>
2013-12-27 13:00:44 -0800

    Create  Python-Basics 2.py

---
+++ Python-Basics 2.py
@@ -0,0 +1,11 @@
+thing = "animal"
+animal = "Cat"
+
+if thing == "animal":
+   if animal == "Cat":
+    print 'This is a cat'
+else:
+    'print i dont know what animal this is'
+
+else:
+    print 'I dont know what thing is'
